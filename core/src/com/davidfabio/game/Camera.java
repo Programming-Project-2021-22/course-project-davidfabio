1b22e4d0c9082f0c58ffd183760c2f3a01d2395f
Fabio Vitalba <fabvit9@gmail.com>
2023-03-26 15:24:41 +0200

    Added Camera movement and fixed y-coordinates in general movement

--- core/src/com/davidfabio/game/Camera.java
+++ core/src/com/davidfabio/game/Camera.java
@@ -7,7 +7,7 @@ public class Camera extends OrthographicCamera {
     private static final float MOVEMENT_SPEED = 80f;
     public Camera() {
         super();
-        this.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        this.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         this.position.set(this.viewportWidth / 2f, this.viewportHeight / 2f, 0);
         this.zoom = 0.5f;   // We start zoomed in
     }
@@ -18,22 +18,31 @@ public class Camera extends OrthographicCamera {
             this.zoom += 0.2f * deltaTime;
         }
         // Then we move towards the player
-        // TODO-Fabio: This needs to be fixed
-        //this.moveTowards(player.getX(), player.getY(), deltaTime);
+        this.moveTowards(player.getX(), player.getY(), deltaTime);
         this.update();
     }
 
     private void moveTowards(float otherX, float otherY, float deltaTime) {
-        float dir = getAngleTowards(otherX, -otherY);
-        moveTowards(dir, deltaTime);
+        float dir = getAngleTowards(otherX, otherY);
+        // Increase camera speed when distance to player is large, so that the player never goes
+        // out of view.
+        float distanceToPlayer = getDistanceTo(otherX, otherY);
+        float speedMultiplier = distanceToPlayer > 240 ? (distanceToPlayer / 240 * 2) : 1.0f;
+        float speed = Camera.MOVEMENT_SPEED * deltaTime * speedMultiplier;
+        moveTowardsWSpeed(dir, deltaTime, speed);
     }
 
     private float getAngleTowards(float otherX, float otherY) {
         return ((float)Math.atan2(otherY - this.position.y, otherX - this.position.x));
     }
 
-    public void moveTowards(float direction, float deltaTime) {
-        float speed = Camera.MOVEMENT_SPEED * deltaTime;
+    public float getDistanceTo(float otherX, float otherY) {
+        float distanceX = this.position.x - otherX;
+        float distanceY = this.position.y - otherY;
+        return (float)Math.sqrt((distanceX * distanceX) + (distanceY * distanceY));
+    }
+
+    public void moveTowardsWSpeed(float direction, float deltaTime, float speed) {
         float deltaX = (float)Math.cos(direction) * speed;
         float deltaY = (float)Math.sin(direction) * speed;
 
