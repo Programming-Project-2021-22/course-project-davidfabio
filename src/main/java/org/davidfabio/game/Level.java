a675be0c255e1da6359635582036de6098754119
david-pittracher <65851393+david-pittracher@users.noreply.github.com>
2023-04-22 19:27:08 +0200

    Added stars background.

--- src/main/java/org/davidfabio/game/Level.java
+++ src/main/java/org/davidfabio/game/Level.java
@@ -3,17 +3,43 @@ package org.davidfabio.game;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import org.davidfabio.utils.Settings;
+import org.davidfabio.utils.Transform2D;
 
 public class Level {
     private float width;
     private float height;
 
+    private float[] starsX, starsY, starsRadii;
+    private Color[] starsColors;
+    private int starsCount = 256;
+
     public Level(float width, float height) {
         this.width = width;
         this.height = height;
+
+        starsX = new float[starsCount];
+        starsY = new float[starsCount];
+        starsRadii = new float[starsCount];
+        starsColors = new Color[starsCount];
+
+        for (int i = 0; i < starsCount; i += 1) {
+            starsX[i] = Transform2D.getRandomX(this);
+            starsY[i] = Transform2D.getRandomY(this);
+            starsRadii[i] = (float)Math.random() + 0.25f;
+            starsColors[i] = new Color((float)Math.random(), (float)Math.random(), (float)Math.random(), (float)Math.random() - 0.5f);
+        }
     }
 
     public void render(ShapeRenderer renderer) {
+        // background stars
+        renderer.begin(ShapeRenderer.ShapeType.Filled);
+        for (int i = 0; i < starsCount; i += 1) {
+            renderer.setColor(starsColors[i]);
+            renderer.circle(starsX[i], starsY[i], starsRadii[i]);
+        }
+        renderer.end();
+
+        // level border
         renderer.begin(ShapeRenderer.ShapeType.Line);
         renderer.setColor(Color.LIGHT_GRAY);
         renderer.rect(0,0,this.width,this.height);
