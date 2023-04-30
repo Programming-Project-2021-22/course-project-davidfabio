4546e68d3dd2e7dc77b22f17152d530d67adced7
david-pittracher <65851393+david-pittracher@users.noreply.github.com>
2023-04-30 12:19:12 +0200

    .

--- src/main/java/org/davidfabio/game/Sounds.java
+++ src/main/java/org/davidfabio/game/Sounds.java
@@ -21,176 +21,69 @@ public class Sounds {
     private static Sound sfxGameOver;
     private static Sound musicTrack;
 
+    public static void playShootSfx() { playSoundEffect(sfxShoot); }
+    public static void playExplosionSfx() { playSoundEffect(sfxExplosion); }
+    public static void playExplosionEnemyStarSfx() { playSoundEffect(sfxExplosionEnemyStar); }
+    public static void playHitSfx() { playSoundEffect(sfxHit); }
+    public static void playDashSfx() { playSoundEffect(sfxDash); }
+    public static void playPickupSfx() { playSoundEffect(sfxPickup); }
+    public static void playGameStartSfx() { playSoundEffect(sfxGameStart); }
+    public static void playGameOverSfx() { playSoundEffect(sfxGameOver); }
+
+    private static Sound loadSound(String fileName) {
+        Sound sound;
+        try {
+            sound = Gdx.audio.newSound(Gdx.files.internal("src/main/resources/sfx/" + fileName));
+        }
+        catch (GdxRuntimeException ex) {
+            sound = null;
+            System.out.println("Sound could not be loaded.\n" + ex.getMessage());
+        }
+        return sound;
+    }
 
     public static void loadSounds() {
         if (Settings.sfxEnabled) {
-            if(sfxShoot == null) {
-                try {
-                    sfxShoot = Gdx.audio.newSound(Gdx.files.internal("src/main/resources/sfx/shoot.wav"));
-                }
-                catch (GdxRuntimeException ex) {
-                    sfxShoot = null;
-                    System.out.println("Sound could not be loaded.\n" + ex.getMessage());
-                }
-            }
+            sfxShoot = loadSound("shoot.wav");
+            sfxExplosion = loadSound("explosion.wav");
+            sfxExplosionEnemyStar = loadSound("explosion_enemy_star.wav");
+            sfxHit = loadSound("bullet_hit.wav");
+            sfxDash = loadSound("dash.wav");
+            sfxPickup = loadSound("pickup.wav");
+            sfxGameStart = loadSound("game_start.wav");
+            sfxGameOver = loadSound("game_over.wav");
         }
-
-        if (Settings.sfxEnabled) {
-            if(sfxShoot == null) {
-                try {
-                    sfxShoot = Gdx.audio.newSound(Gdx.files.internal("src/main/resources/sfx/shoot.wav"));
-                }
-                catch (GdxRuntimeException ex) {
-                    sfxShoot = null;
-                    System.out.println("Sound could not be loaded.\n" + ex.getMessage());
-                }
-            }
-
-            if(sfxExplosion == null) {
-                try {
-                    sfxExplosion = Gdx.audio.newSound(Gdx.files.internal("src/main/resources/sfx/explosion.wav"));
-                }
-                catch (GdxRuntimeException ex) {
-                    sfxExplosion = null;
-                    System.out.println("Sound could not be loaded.\n" + ex.getMessage());
-                }
-            }
-
-            if(sfxHit == null) {
-                try {
-                    sfxHit = Gdx.audio.newSound(Gdx.files.internal("src/main/resources/sfx/bullet_hit.wav"));
-                }
-                catch (GdxRuntimeException ex) {
-                    sfxHit = null;
-                    System.out.println("Sound could not be loaded.\n" + ex.getMessage());
-                }
-            }
-
-            if(sfxDash == null) {
-                try {
-                    sfxDash = Gdx.audio.newSound(Gdx.files.internal("src/main/resources/sfx/dash.wav"));
-                }
-                catch (GdxRuntimeException ex) {
-                    sfxDash = null;
-                    System.out.println("Sound could not be loaded.\n" + ex.getMessage());
-                }
-            }
-
-            if(sfxPickup == null) {
-                try {
-                    sfxPickup = Gdx.audio.newSound(Gdx.files.internal("src/main/resources/sfx/pickup.wav"));
-                }
-                catch (GdxRuntimeException ex) {
-                    sfxPickup = null;
-                    System.out.println("Sound could not be loaded.\n" + ex.getMessage());
-                }
-            }
-
-            if(sfxExplosionEnemyStar == null) {
-                try {
-                    sfxExplosionEnemyStar = Gdx.audio.newSound(Gdx.files.internal("src/main/resources/sfx/explosion_enemy_star.wav"));
-                }
-                catch (GdxRuntimeException ex) {
-                    sfxExplosionEnemyStar = null;
-                    System.out.println("Sound could not be loaded.\n" + ex.getMessage());
-                }
-            }
-
-            if(sfxGameStart == null) {
-                try {
-                    sfxGameStart = Gdx.audio.newSound(Gdx.files.internal("src/main/resources/sfx/game_start.wav"));
-                }
-                catch (GdxRuntimeException ex) {
-                    sfxGameStart = null;
-                    System.out.println("Sound could not be loaded.\n" + ex.getMessage());
-                }
-            }
-
-            if(sfxGameOver == null) {
-                try {
-                    sfxGameOver = Gdx.audio.newSound(Gdx.files.internal("src/main/resources/sfx/game_over.wav"));
-                }
-                catch (GdxRuntimeException ex) {
-                    sfxGameOver = null;
-                    System.out.println("Sound could not be loaded.\n" + ex.getMessage());
-                }
-            }
-
-        }
-
 
         if (Settings.musicEnabled) {
-            if (musicTrack == null ) {
-                try {
-                    // Note: Loading Music still takes very long.
-                    // However, this is done once and not more often.
-                    musicTrack = Gdx.audio.newSound(Gdx.files.internal("src/main/resources/music/track1.mp3"));
-                }
-                catch (GdxRuntimeException ex) {
-                    musicTrack = null;
-                    System.out.println("Sound could not be loaded.\n" + ex.getMessage());
-                }
-            }
+            musicTrack = loadSound("track1.mp3");
         }
     }
 
-    public static void playHitSfx() {
-        if ((sfxHit != null) && (Settings.sfxEnabled)) {
-            sfxHit.play(Settings.sfxVolume);
-        }
-    }
+    private static void playSoundEffect(Sound sound) {
+        if (!Settings.sfxEnabled)
+            return;
+        if (sound == null)
+            return;
 
-    public static void playShootSfx() {
-        if ((sfxExplosion != null) && (Settings.sfxEnabled)) {
-            sfxShoot.play(Settings.sfxVolume);
-        }
-    }
-
-    public static void playDashSfx() {
-        if ((sfxDash != null) && (Settings.sfxEnabled)) {
-            sfxDash.play(Settings.sfxVolume);
-        }
-    }
-
-    public static void playPickupSfx() {
-        if ((sfxPickup != null) && (Settings.sfxEnabled)) {
-            sfxPickup.play(Settings.sfxVolume);
-        }
-    }
-
-    public static void playExplosionSfx() {
-        if ((sfxExplosion != null) && (Settings.sfxEnabled)) {
-            sfxExplosion.play(Settings.sfxVolume);
-        }
-    }
-
-    public static void playExplosionEnemyStarSfx() {
-        if ((sfxExplosionEnemyStar != null) && (Settings.sfxEnabled)) {
-            sfxExplosionEnemyStar.play(Settings.sfxVolume);
-        }
-    }
-
-    public static void playGameStartSfx() {
-        if ((sfxGameStart != null) && (Settings.sfxEnabled)) {
-            sfxGameStart.play(Settings.sfxVolume);
-        }
-    }
-
-    public static void playGameOverSfx() {
-        if ((sfxGameOver != null) && (Settings.sfxEnabled)) {
-            sfxGameOver.play(Settings.sfxVolume);
-        }
+        sound.play(Settings.sfxVolume);
     }
 
     public static void playBackgroundMusic() {
-        if ((musicTrack != null) && (Settings.musicEnabled)) {
-            musicTrack.loop(Settings.musicVolume);
-        }
+        if (!Settings.musicEnabled)
+            return;
+        if (musicTrack == null)
+            return;
+
+        musicTrack.loop(Settings.musicVolume);
     }
 
     public static void stopBackgroundMusic() {
-        if ((musicTrack != null) && Settings.musicEnabled) {
-            musicTrack.stop();
-        }
+        if (!Settings.musicEnabled)
+            return;
+        if (musicTrack == null)
+            return;
+
+        musicTrack.stop();
     }
+
 }
