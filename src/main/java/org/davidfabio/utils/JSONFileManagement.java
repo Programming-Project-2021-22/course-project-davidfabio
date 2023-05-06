5f7a483b0f6fb3015a8c07b03b087d8a590458bf
Fabio Vitalba <fabvit9@gmail.com>
2023-05-06 16:43:00 +0200

    Added Documentation for JSONFileManagement

--- src/main/java/org/davidfabio/utils/JSONFileManagement.java
+++ src/main/java/org/davidfabio/utils/JSONFileManagement.java
@@ -1,6 +1,7 @@
 package org.davidfabio.utils;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.davidfabio.game.Score;
@@ -9,27 +10,51 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * This class provides utility functions to read & write from json-files.
+ * In particular this class is used to read and write settings and scores.
+ */
 public class JSONFileManagement {
+    /**
+     * This method takes a .json-File and reads the contents. The Settings found
+     * inside are copied into the static class {@link Settings}.
+     * @param settingsFile .json-File that contains the game settings.
+     */
     public static void initSettingsFromFile(File settingsFile) {
         ObjectMapper mapper = new ObjectMapper();
         try {
-            ObjectNode settings = mapper.readValue(settingsFile, ObjectNode.class);
-            Settings.username = settings.get("username").asText();
-            Settings.levelWidth = (float)settings.get("levelWidth").asDouble();
-            Settings.levelHeight = (float)settings.get("levelHeight").asDouble();
-            Settings.fullscreenEnabled = settings.get("fullscreenEnabled").asBoolean();
-            Settings.windowWidth = settings.get("windowWidth").asInt();
-            Settings.windowHeight = settings.get("windowHeight").asInt();
-            Settings.sfxEnabled = settings.get("sfxEnabled").asBoolean();
-            Settings.musicEnabled = settings.get("musicEnabled").asBoolean();
-            Settings.sfxVolume = (float)settings.get("sfxVolume").asDouble();
-            Settings.musicVolume = (float)settings.get("musicVolume").asDouble();
+            JsonNode settings = mapper.readValue(settingsFile, JsonNode.class);
+            if (settings.has("username"))
+                Settings.username = settings.get("username").asText();
+            if (settings.has("levelWidth"))
+                Settings.levelWidth = (float)settings.get("levelWidth").asDouble();
+            if (settings.has("levelHeight"))
+                Settings.levelHeight = (float)settings.get("levelHeight").asDouble();
+            if (settings.has("fullscreenEnabled"))
+                Settings.fullscreenEnabled = settings.get("fullscreenEnabled").asBoolean();
+            if (settings.has("windowWidth"))
+                Settings.windowWidth = settings.get("windowWidth").asInt();
+            if (settings.has("windowHeight"))
+                Settings.windowHeight = settings.get("windowHeight").asInt();
+            if (settings.has("sfxEnabled"))
+                Settings.sfxEnabled = settings.get("sfxEnabled").asBoolean();
+            if (settings.has("musicEnabled"))
+                Settings.musicEnabled = settings.get("musicEnabled").asBoolean();
+            if (settings.has("sfxVolume"))
+                Settings.sfxVolume = (float)settings.get("sfxVolume").asDouble();
+            if (settings.has("musicVolume"))
+                Settings.musicVolume = (float)settings.get("musicVolume").asDouble();
         } catch (IOException e) {
             // do nothing
             e.printStackTrace();
         }
     }
 
+    /**
+     * This method writes the mutable values of the {@link Settings} class into
+     * a .json File.
+     * @param settingsFile .json-file where the settings are written to.
+     */
     public static void writeSettingsToFile(File settingsFile) {
         ObjectMapper mapper = new ObjectMapper();
         ObjectNode settings = mapper.createObjectNode();
@@ -51,6 +76,12 @@ public class JSONFileManagement {
         }
     }
 
+    /**
+     * Reads all the scores stored in a .json-file. These scores are returned in an
+     * {@link ArrayList}.
+     * @param scoresFile .json-file containing all the past scores
+     * @return an ArrayList containing {@link Score}s found in the file
+     */
     public static ArrayList<Score> initScoresFromFile(File scoresFile) {
         ObjectMapper mapper = new ObjectMapper();
         try {
@@ -61,6 +92,11 @@ public class JSONFileManagement {
         }
     }
 
+    /**
+     * Stores the provided scores into a .json-file.
+     * @param scoresFile file where the scores are saved to.
+     * @param scores the scores that need to be stored.
+     */
     public static void writeScoresToFile(File scoresFile, ArrayList<Score> scores) {
         ObjectMapper mapper = new ObjectMapper();
         try {
