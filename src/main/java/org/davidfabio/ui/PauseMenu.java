9a6725db5f3e721cbc7ea3afc6852213ef88fc95
Fabio Vitalba <fabvit9@gmail.com>
2023-05-06 21:21:05 +0200

    Added documentation to PauseMenu

--- src/main/java/org/davidfabio/ui/PauseMenu.java
+++ src/main/java/org/davidfabio/ui/PauseMenu.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
@@ -15,21 +14,32 @@ import org.davidfabio.PolygonWars;
 import org.davidfabio.game.Score;
 
 import java.util.ArrayList;
-import java.util.function.Consumer;
 
+/**
+ * This class is used to display a Pause Menu during the Game Loop.
+ */
 public class PauseMenu extends Group {
+    /**
+     * This is used as a basis for the Pause Menu Screen. It contains all controls.
+     */
     private Table mainTable;
-    private ArrayList<Score> scores;
 
+    /**
+     * Creates a new PauseMenu object and loads the Skin using {@link UIBuilder#loadSkin()}.
+     */
     public PauseMenu() {
         super();
         setBounds(0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
         UIBuilder.loadSkin();
     }
 
+    /**
+     * This method builds the PauseMenu screen. It adds all Buttons and Labels that are available to the user.
+     *
+     * @param scores all past game scores that need to be passed on to other screens.
+     * @param onResume action to be executed in order to get back to the Game.
+     */
     public void init(ArrayList<Score> scores, Runnable onResume) {
-        this.scores = scores;
-
         Pixmap pixmapBackground = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
         pixmapBackground.setColor(Color.DARK_GRAY);
         pixmapBackground.fill();
