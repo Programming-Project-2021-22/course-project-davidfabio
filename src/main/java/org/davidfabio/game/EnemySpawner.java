1bc154cd6f17a652149567967cffb45cb46c43e6
david-pittracher <65851393+david-pittracher@users.noreply.github.com>
2023-05-14 20:09:04 +0200

    .

--- src/main/java/org/davidfabio/game/EnemySpawner.java
+++ src/main/java/org/davidfabio/game/EnemySpawner.java
@@ -7,178 +7,25 @@ import org.davidfabio.utils.Transform2D;
 import java.util.Random;
 
 public class EnemySpawner {
-
-
-    private int MAX_ACTIVE_ENEMIES = 24;
-    private float DELAY_BETWEEN_SPAWNS = 1.5f;
-
     private float timeElapsed = 0;
     private float timeLastFrame;
-    private float timeElapsedSinceLastSpawn = 0;
-    private float centerX, centerY, rightBorder, bottomBorder;
+    private float centerX, centerY, levelWidth, levelHeight;
     private World worldReference;
     private Level levelReference;
-
     private Random random;
-
-    private enum SpawnFormation {
-        CIRCLE,
-        LINE,
-        RANDOM
-    }
-
 
     public EnemySpawner(World world) {
         worldReference = world;
         levelReference = world.getLevel();
         centerX = worldReference.getLevel().getWidth() / 2;
         centerY = worldReference.getLevel().getHeight() / 2;
-        rightBorder = worldReference.getLevel().getWidth();
-        bottomBorder = worldReference.getLevel().getHeight();
+        levelWidth = worldReference.getLevel().getWidth();
+        levelHeight = worldReference.getLevel().getHeight();
 
         random = new Random();
     }
 
 
-    public void update(float deltaTime) {
-        timeElapsed += deltaTime;
-        timeElapsedSinceLastSpawn += deltaTime;
-
-        if (timeElapsedSinceLastSpawn < DELAY_BETWEEN_SPAWNS)
-            return;
-        if (getActiveEnemiesCount() > MAX_ACTIVE_ENEMIES)
-            return;
-
-
-        // spawn new enemies
-        spawnGroup();
-        timeElapsedSinceLastSpawn = 0f;
-    }
-
-
-
-
-    private Enemy spawn(Enemy.Type type, float x, float y) {
-        Enemy enemy = worldReference.getEnemy(type);
-        if (enemy == null)
-            return null;
-
-        switch (type) {
-            case CHASER:   enemy.init(x, y, 50, 100, 2, new Color(1, 0, 0, 0.75f)); break;
-            case TURRET:   enemy.init(x, y, 60, 0, 10, Color.BLUE); break;
-            case BUBBLE:   enemy.init(x, y, 160, 20, 9, new Color(1, 0.75f, 0.8f, 0.75f)); break;
-            case KAMIKAZE: enemy.init(x, y, 30, 100, 1, Color.ORANGE); break;
-            case STAR:     enemy.init(x, y, 80, 30, 15, Color.PURPLE); break;
-        }
-
-        return enemy;
-    }
-
-
-
-
-    private void spawnCircle() {
-        Enemy.Type enemyType = getRandomEnemyType();
-        int enemyCount = getEnemyCount(enemyType);
-
-        float radius = random.nextFloat(64, 256);
-        float centerX = random.nextFloat(0, rightBorder);
-        float centerY = random.nextFloat(0, bottomBorder);
-
-        float angleDelta = (float)(2 * Math.PI) / enemyCount;
-        for (int i = 0; i < enemyCount; i += 1) {
-            float x = Transform2D.translateX(centerX, (i * angleDelta), radius);
-            float y = Transform2D.translateY(centerY, (i * angleDelta), radius);
-            spawn(enemyType, x, y);
-        }
-    }
-
-
-    private void spawnLine() {
-        Enemy.Type enemyType = getRandomEnemyType();
-        int enemyCount = getEnemyCount(enemyType);
-        float minLength = 200f;
-        float x1 = 0, y1 = 0, x2 = 0, y2 = 0;
-
-        while (Transform2D.getDistance(x1, y1, x2, y2) < minLength) {
-            x1 = random.nextFloat(0, levelReference.getWidth());
-            x2 = random.nextFloat(0, levelReference.getWidth());
-            y1 = random.nextFloat(0, levelReference.getHeight());
-            y2 = random.nextFloat(0, levelReference.getHeight());
-        }
-
-        float xOffset = Math.abs(x2 - x1) / enemyCount;
-        float yOffset = Math.abs(y2 - y1) / enemyCount;
-
-        for (int i = 0; i < enemyCount; i += 1) {
-            float x = x1 + (i * xOffset);
-            float y = y1 + (i * yOffset);
-            spawn(enemyType, x, y);
-        }
-    }
-
-    private void spawnRandom() {
-        Enemy.Type enemyType = getRandomEnemyType();
-        int enemyCount = getEnemyCount(enemyType);
-
-        for (int i = 0; i < enemyCount; i += 1) {
-            float x = random.nextFloat(0, rightBorder);
-            float y = random.nextFloat(0, bottomBorder);
-            spawn(enemyType, x, y);
-        }
-    }
-
-
-    private void spawnGroup() {
-        SpawnFormation spawnFormation = getRandomSpawnType();
-        switch (spawnFormation) {
-            case CIRCLE: spawnCircle(); break;
-            case LINE: spawnLine(); break;
-            case RANDOM: spawnRandom(); break;
-        }
-    }
-
-
-    private int getEnemyCount(Enemy.Type enemyType) {
-        int enemyCount = 1;
-        switch (enemyType) {
-            case CHASER:   enemyCount = random.nextInt(6, 12); break;
-            case TURRET:   enemyCount = random.nextInt(1, 3); break;
-            case BUBBLE:   enemyCount = random.nextInt(1, 3); break;
-            case KAMIKAZE: enemyCount = random.nextInt(6, 22); break;
-            case STAR:     enemyCount = 1; break;
-        }
-
-        return enemyCount;
-    }
-
-    private int getActiveEnemiesCount() {
-        int res = 0;
-        for (Enemy enemy : worldReference.getEnemies())
-            if (enemy.getIsActive())
-                res += 1;
-
-        return  res;
-    }
-
-    private Enemy.Type getRandomEnemyType() {
-        int enemyTypesCount = Enemy.Type.values().length;
-        int rand = random.nextInt(enemyTypesCount);
-        return Enemy.Type.values()[rand];
-    }
-
-    private SpawnFormation getRandomSpawnType() {
-        int spawnTypesCount = SpawnFormation.values().length;
-        int rand = random.nextInt(spawnTypesCount);
-        return SpawnFormation.values()[rand];
-    }
-
-    
-
-
-
-
-    /*
     private Enemy spawn(Enemy.Type type, float x, float y, float spawnTime) {
         if (timeLastFrame >= spawnTime || timeElapsed < spawnTime)
             return null;
@@ -214,8 +61,8 @@ public class EnemySpawner {
     }
 
     private void spawnAtRandomPoint(Enemy.Type type, float spawnTime) {
-        float x = Transform2D.getRandomX(levelReference);
-        float y = Transform2D.getRandomY(levelReference);
+        float x = random.nextFloat(0, levelWidth);
+        float y = random.nextFloat(0, levelHeight);
         Enemy enemy = spawn(type, x, y, spawnTime);
 
         if (enemy != null)
@@ -231,10 +78,35 @@ public class EnemySpawner {
             spawn(type, x, y, spawnTime + (i * timeDeltaBetweenEnemies));
         }
     }
-     */
 
+
+    public void update(float deltaTime) {
+        timeElapsed += deltaTime;
+        timeLastFrame = timeElapsed - deltaTime;
+
+        spawnAtRandomPoint(Enemy.Type.STAR, 5f);
+        spawnAtRandomPoint(Enemy.Type.STAR, 15f);
+        spawnAtRandomPoint(Enemy.Type.STAR, 30f);
+        spawnAtRandomPoint(Enemy.Type.STAR, 40f);
+
+        spawnGroupAtLine(Enemy.Type.CHASER, 16, 0, 0, levelWidth, 0, 1f, 0);
+        spawnGroupAtLine(Enemy.Type.KAMIKAZE, 16, 0, 0, levelWidth, levelHeight, 5f, 0);
+        spawnGroupInCircle(Enemy.Type.CHASER, 12, centerX, centerY, 260, 8, 0.15f);
+
+        spawnGroupInCircle(Enemy.Type.BUBBLE, 4, centerX, centerY, 250, 13f, 0.5f);
+        spawn(Enemy.Type.TURRET, levelWidth - 50, levelHeight - 50, 19f);
+        spawn(Enemy.Type.TURRET, levelWidth - 50, 50, 15f);
+        spawn(Enemy.Type.TURRET, 50, levelHeight - 50, 17f);
+
+        spawnGroupAtPoint(Enemy.Type.KAMIKAZE, 6, 100, 100, 25, 0.5f);
+        spawnGroupAtPoint(Enemy.Type.KAMIKAZE, 6, levelWidth - 100, 100, 28, 0.5f);
+        spawnGroupAtPoint(Enemy.Type.KAMIKAZE, 6, 100, levelHeight - 100, 32, 0.5f);
+        spawnGroupAtPoint(Enemy.Type.KAMIKAZE, 6, levelWidth - 100, levelHeight - 100, 32, 0.5f);
+
+        spawnGroupInCircle(Enemy.Type.CHASER, 20, 50, 50, 200, 32, 0);
+        spawnGroupInCircle(Enemy.Type.CHASER, 20, levelWidth - 50, levelHeight - 50, 200, 32, 0);
+        spawnGroupInCircle(Enemy.Type.KAMIKAZE, 32, centerX, centerY, 150, 38, 0);
+        spawnGroupInCircle(Enemy.Type.KAMIKAZE, 32, centerX, centerY, 150, 41, 0);
+
+    }
 }
-
-
-
-
