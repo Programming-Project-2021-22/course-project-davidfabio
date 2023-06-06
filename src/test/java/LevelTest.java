6c67f78653c7acf8e7d2e918dfd0760dd8f0e299
Fabio Vitalba <fabvit9@gmail.com>
2023-06-06 21:17:12 +0200

    Add Level Tests

---
+++ src/test/java/LevelTest.java
@@ -0,0 +1,18 @@
+import org.davidfabio.game.Camera;
+import org.davidfabio.game.Level;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LevelTest {
+    @Test
+    @DisplayName("Create new Level")
+    public void testCreateScore() {
+        float width = 100, height = 100;
+        Level level = new Level(width,height);
+
+        assertEquals(level.getWidth(),width);
+        assertEquals(level.getHeight(),height);
+    }
+}
