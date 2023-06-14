247c71e3e08f2be3aa049633b21ac84577a51dcf
Fabio Vitalba <fabvit9@gmail.com>
2023-06-14 20:00:10 +0200

    Add documentation for Mouse class

--- src/main/java/org/davidfabio/input/Mouse.java
+++ src/main/java/org/davidfabio/input/Mouse.java
@@ -5,11 +5,36 @@ import com.badlogic.gdx.math.Vector3;
 import org.davidfabio.game.Camera;
 import org.davidfabio.ui.GameScreen;
 
+/**
+ * This class is used to translate the actual Mouse-position to the Worlds-coordinates.
+ * This is required because the {@link Camera} distorts the mouse position, and therefore we need to retrieve
+ * the "actual" mouse position for many functionalities.
+ */
 public class Mouse {
+    /**
+     * Contains the "actual" mouse position
+     */
     private static int x, y; // top-left of the window is 0,0 -- bottom-right is windowWidth,windowHeight
-    public static int getX() { return x; }
-    public static int getY() { return y; }
 
+    /**
+     * Returns the x-position for the Mouse class
+     * @return x-position
+     */
+    public static int getX() {
+        return x;
+    }
+
+    /**
+     * Returns the y-position for the Mouse class
+     * @return y-position
+     */
+    public static int getY() {
+        return y;
+    }
+
+    /**
+     * Updates the Mouse position based on the position on screen and the current camera-projection.
+     */
     static void update() {
         Camera gameCamera = GameScreen.getCamera();
         if (gameCamera != null) {
@@ -21,5 +46,4 @@ public class Mouse {
             y = Gdx.input.getY();
         }
     }
-
 }
