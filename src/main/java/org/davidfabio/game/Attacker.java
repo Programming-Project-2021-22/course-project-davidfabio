9ba035a35fb7bb3a3bec3efc07690644b936bd6f
Fabio Vitalba <fabvit9@gmail.com>
2023-06-18 22:09:00 +0200

    Add documentation to Attacker interface

--- src/main/java/org/davidfabio/game/Attacker.java
+++ src/main/java/org/davidfabio/game/Attacker.java
@@ -3,9 +3,26 @@ package org.davidfabio.game;
 import com.badlogic.gdx.graphics.Color;
 import org.davidfabio.game.enemies.EnemyStar;
 
+/**
+ * This interface is used to identify any Class/Object that should be able to attack a Class/Object that implements the
+ * {@link Attackable} interface.
+ */
 public interface Attacker {
+    /**
+     * @return The damage that is applied upon attack.
+     */
     int getAttackPower();
 
+    /**
+     * This method implements a default behaviour for attacks. It usually takes an {@link Attackable} object and
+     * the {@link World} object in order to process any side-effects.
+     * <p>If the Player is the {@link Attackable}, the method verifies that the Player is not dashing or currently otherwise
+     * invincible.
+     * <p>If the EnemyStar is the {@link Attackable}, the method verifies that the Enemy is not about to blow up.
+     *
+     * @param attackable Object that will be attacked (that takes damage)
+     * @param world World reference, used for side-effects
+     */
     default void attack(Attackable attackable, World world) {
         if (!attackable.getIsActive())
             return;
